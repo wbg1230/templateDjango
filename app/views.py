fa9c79168e2a1254c51db8753883a123c4d06e20
wbg1230 <96736758+wbg1230@users.noreply.github.com>
2022-03-03 13:02:31 +0100

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import ObjectDoesNotExist
+
+
+def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login.html")
+    return html(request, "index")
+
+
+def html(request, filename):
+    context = {"filename": filename,
+               "collapse": ""}
+    if request.user.is_anonymous and filename != "login":
+        return redirect("/login.html")
+    if filename == "logout":
+        logout(request)
+        return redirect("/")
+    if filename == "login" and request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        try:
+            if "@" in username:
+                user = User.objects.get(email=username)
+            else:
+                user = User.objects.get(username=username)
+            user = authenticate(request, username=user.username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                context["error"] = "Wrong password"
+        except ObjectDoesNotExist:
+            context["error"] = "User not found"
+
+        print("login")
+        print(username, password)
+    print(filename, request.method)
+    if filename in ["buttons", "cards"]:
+        context["collapse"] = "components"
+    if filename in ["utilities-color", "utilities-border", "utilities-animation", "utilities-other"]:
+        context["collapse"] = "utilities"
+    if filename in ["404", "blank"]:
+        context["collapse"] = "pages"
+
+    return render(request, f"{filename}.html", context=context)
